53e029f1e4d9d6a09f84f616b88df82c5e26ab31
Curso <Curso@GBRB9.ipartekaula.com>
2015-12-23 13:20:03 +0100

    Insertados 10003 registros y 1 error

--- src/migracion/Migracion.java
+++ src/migracion/Migracion.java
@@ -7,7 +7,7 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
-
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -15,7 +15,7 @@ import java.sql.SQLException;
 public class Migracion {
 
 	static File dir; // carpeta temporal para las pruebas
-	static final String ARCHIVO_PERSONAS = "datos/personas10.txt";
+	static final String ARCHIVO_PERSONAS = "datos/personas.txt";
 	static final String ARCHIVO_RESUMEN = "datos/resumen.txt";
 	static int numeroInsercciones = 0;
 	static int numeroErrores = 0;
@@ -36,7 +36,9 @@ public class Migracion {
 
 			String[] aCampos;
 			String linea = null;
-			DbConnection conn = new DbConnection();
+			//Abrir conexion
+			DbConnection db = new DbConnection();
+			Connection conexion = db.getConnection();
 			PreparedStatement pst;
 			//Leer el fichero e insertar en la base de datos
 			while ((linea = br.readLine()) != null) {
@@ -48,7 +50,7 @@ public class Migracion {
 					// sentencia sql
 					String sql = "INSERT INTO `persona` (`nombre`,`email`,`observaciones`) VALUES (?,?,?);";
 					// creamos consulta
-					pst = conn.getConnection().prepareStatement(sql,PreparedStatement.RETURN_GENERATED_KEYS);
+					pst = conexion.prepareStatement(sql,PreparedStatement.RETURN_GENERATED_KEYS);
 					pst.setString(1, aCampos[0] + " " + aCampos[1] + " "+ aCampos[2]);
 					pst.setString(2, aCampos[4]);
 					pst.setString(3, aCampos[6]);
@@ -56,24 +58,20 @@ public class Migracion {
 					// ejecutar la consulta
 
 					if (pst.executeUpdate() == 1) {
-
-						ResultSet generatedKeys = pst.getGeneratedKeys();
-						if (generatedKeys.next()) {
-							generatedKeys.getInt(1);
-							numeroInsercciones++;
-						} else {
-							throw new SQLException("Creating user failed, no ID obtained.");
-						}
-
+						System.out.println(numeroInsercciones + " - " + linea);
+						numeroInsercciones++;
+					}else{
+						System.out.println("*** ERROR ");
+						numeroErrores++;
 					}
 					pst.close();
 
 				}
 
-			}
+			}//end while
+			
 			// Cerramos conexión
-
-			conn.desconectar();
+			db.desconectar();
 
 		} catch (FileNotFoundException e) {
 			System.out.println("No se ha encontrado el fichero " + dir);
